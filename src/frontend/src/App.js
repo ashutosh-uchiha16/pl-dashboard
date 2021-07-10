5bb2fba6108ab4e355670ddc34e3d6451d9adcaf
ashutosh-uchiha16 <ashutoshn16@gmail.com>
2021-07-09 21:16:07 -0500

    React App Team Page

---
+++ src/frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { TeamPage } from "./pages/TeamPage";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Premier League Dashboard</h1>
+      <TeamPage />
+    </div>
+  );
+}
+
+export default App;
--- src/main/java/io/code/pldashboard/controller/TeamController.java
+++ src/main/java/io/code/pldashboard/controller/TeamController.java
@@ -1,6 +1,7 @@
 package io.code.pldashboard.controller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import io.code.pldashboard.repository.MatchRepository;
 import io.code.pldashboard.repository.TeamRepository;
 
 @RestController
+@CrossOrigin
 public class TeamController {
     
 
